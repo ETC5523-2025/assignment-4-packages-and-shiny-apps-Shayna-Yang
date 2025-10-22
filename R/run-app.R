e98a33d9dea3a6d50cc20061fe9222885c027716
Shayna Yang <tyan0082@student.monash.edu>
2025-10-23 00:26:08 +1100

    Update pacjage name

--- R/run-app.R
+++ R/run-app.R
@@ -1,5 +1,5 @@
 #' @export
 run_app <- function() {
-  app_dir <- system.file("shiny-app", package = "BHAIExplore")
+  app_dir <- system.file("shiny-app", package = "shayhai")
   shiny::runApp(app_dir, display.mode = "normal")
 }
